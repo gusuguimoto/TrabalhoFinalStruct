0ebdb4bd5e6902dfeac5a109e70fd40f952c2203
Gustavo Suguimoto <91958415+gusuguimoto@users.noreply.github.com>
2022-02-19 18:13:02 -0300

    Update TrabalhoFinal.c

--- TrabalhoFinal.c
+++ TrabalhoFinal.c
@@ -2,45 +2,72 @@
 #include <stdlib.h>
 #include <math.h>
 int main(){
-  int i, j, NAval, NAluno;
-  float somaAl[j], somaAv[j], somaDP[j], notas[j], mediaAl[j], mediaAv[j], desvio[j];
-  float *nota;
+  int i, j, avaliacoes, alunos, somaAv[i], somaAl[j];
+  float soma, mediaDP, desvio, sigma;
+  float* media;
+  float** nota;
 
   printf("Digite a quantidade de avaliacoes: ");
-  scanf("%d", &NAval);
+  scanf("%d", &avaliacoes);
   printf("Digite a quantidade de alunos: ");
-  scanf("%d", &NAluno);
+  scanf("%d", &alunos);
 
-  nota=(float*)malloc(16*sizeof(float));
+  nota=malloc(alunos*sizeof(float*));
 
-  if(nota==NULL){
-    printf("Error: Memoria insuficiente! \n");
-    exit(1);
-  }
+  for (i=0; i<alunos; i++)
+    nota[i] = malloc (avaliacoes*sizeof(float)) ;
 
-  for(i=0; i<NAval; i++){
+  for(i=0; i<avaliacoes; i++){
     printf("\n----- Avaliacao %d -----\n", i+1);
-    for(j=0; j<NAluno; j++){
+    for(j=0; j<alunos; j++){
       printf("Nota do aluno [%d]: ", j+1);
-      scanf("%f", &nota[j], &nota[i]);
-      somaAl[j] = somaAl[j] + nota[j];
-      somaAv[i] = somaAv[i] + nota[i];
+      scanf("%f", &nota[j][i]);
     }
   }
-
-  printf("\nResultado Final\n");
-  printf("Medias arredondadas, para cima, dos alunos: \n");
-  for(j=0; j<NAluno; j++){
-    mediaAl[j] = somaAl[j]/NAval;
-    printf("Aluno [%d]-> %.2f\n", j+1, ceil(mediaAl[j]));
+  printf("\n\n --- Notas imputadas ---");
+  for(i=0; i<avaliacoes; i++){
+    for(j=0; j<alunos; j++){
+        printf("\n ----- Avaliacao [%d] -----\nNota do aluno [%d]: %.2f", i+1, j+1, nota[j][i]);
     }
+    printf("\n");
+  }
+  printf("\n\n --- Resultado Final --- \n");
+  printf(">Medias arredondadas para cima<");
+  media=malloc(alunos*sizeof(float));
 
-  printf("\nMedia e desvio padrao(populacional) das avaliacoes: \n");
-  for(i=0; i<NAval; i++){
-    mediaAv[i] = somaAv[i]/NAluno;
-    desvio[i] = desvio[i] + pow(notas[i]-mediaAv[i],2);
-    printf("Desvio padrao da avaliacao [%d]: %.2f\n", i+1, sqrt(desvio[i])/NAluno);
+  for(i=0; i<alunos; i++){
+    soma=0.0;
+    for(j=0; j<avaliacoes; j++){
+      soma += nota[i][j];
+      media[i]=soma/avaliacoes;
+    }
+    printf("\nMedia do aluno [%d]: %.2f", i+1, ceil(media[i]));
+  }
+
+  printf("\n");
+  printf("\nMedia e desvio padrao(Populacional) das avaliacoes: ");
+  for(i=0; i<avaliacoes; i++){
+    soma=0.0;
+    for(j=0; j<alunos; j++){
+      soma+=nota[j][i];
+      media[i]=soma/alunos;;
+    }
+    printf("\nAvaliacao [%d]: \nMedia: %.2f", i+1, media[i]);
+  }
+
+  for(j=0; j<avaliacoes; j++){
+    soma=0.0;
+    for(i=0; i<alunos; i++){
+      soma+=nota[i][j];
+    }
+    mediaDP=soma/alunos;
+
+    sigma=0;
+    for(i=0; i<alunos; i++){
+      sigma += pow(nota[i][j]-mediaDP,2);
+    }
+    desvio=sqrt(sigma/alunos);
+    printf("\nDesvio padrao da avaliacao [%d]: %.2f", i+1, desvio);
   }
   free(nota);
-  return 0;
 }
