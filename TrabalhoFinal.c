c03741b8792651a16395af5e8bf0de42ae2fc9ef
Gustavo Suguimoto <91958415+gusuguimoto@users.noreply.github.com>
2022-02-20 15:45:00 -0300

    Update TrabalhoFinal.c

--- TrabalhoFinal.c
+++ TrabalhoFinal.c
@@ -22,6 +22,10 @@ int main(){
     for(j=0; j<alunos; j++){
       printf("Nota do aluno [%d]: ", j+1);
       scanf("%f", &nota[j][i]);
+      if(nota[j][i]>10 || nota[j][i]<0){
+        printf("\n\nERROR: VALOR DE NOTA INVALIDO!\n\n");
+        exit(1);
+      }
     }
   }
   printf("\n\n --- Notas imputadas ---");
@@ -44,30 +48,22 @@ int main(){
     printf("\nMedia do aluno [%d]: %.2f", i+1, ceil(media[i]));
   }
 
-  printf("\n");
-  printf("\nMedia e desvio padrao(Populacional) das avaliacoes: ");
-  for(i=0; i<avaliacoes; i++){
-    soma=0.0;
-    for(j=0; j<alunos; j++){
-      soma+=nota[j][i];
-      media[i]=soma/alunos;;
-    }
-    printf("\nAvaliacao [%d]: \nMedia: %.2f", i+1, media[i]);
-  }
-
+  printf("\n\n");
+  printf("\n>Media e desvio padrao(Populacional) das avaliacoes<  \n");
   for(j=0; j<avaliacoes; j++){
     soma=0.0;
     for(i=0; i<alunos; i++){
       soma+=nota[i][j];
     }
-    mediaDP=soma/alunos;
-
+    media[j]=soma/alunos;
     sigma=0;
     for(i=0; i<alunos; i++){
-      sigma += pow(nota[i][j]-mediaDP,2);
+      sigma += pow(nota[i][j]-media[j],2);
     }
     desvio=sqrt(sigma/alunos);
-    printf("\nDesvio padrao da avaliacao [%d]: %.2f", i+1, desvio);
+    printf("\nAvaliacao [%d]->   Media: %.2f", j+1, media[j]);
+    printf("\tDesvio padrao: %.2f\n", desvio);
   }
+  printf("\n");
   free(nota);
 }
